da0777bdecf2d6f0045a8268249e4e6333b2d3a2
andrey.mikhailov <andynik76@yandex.ru>
2022-08-26 09:56:07 +0300

    Increment 5. Тестирование 1.025.01

--- internal/handlers/server.go
+++ internal/handlers/server.go
@@ -203,7 +203,9 @@ func (rs *RepStore) HandlerUpdateMetricJSON(rw http.ResponseWriter, rq *http.Req
 		return
 	}
 
+	fmt.Println("Пришла метрика", v.ID, v.MType, v.Value, v.Delta)
 	errStatus := rs.SetValueInMapJSON(v)
+	fmt.Println("Статус установки значений метрики", errStatus)
 
 	switch errStatus {
 	case 400:
@@ -215,6 +217,7 @@ func (rs *RepStore) HandlerUpdateMetricJSON(rw http.ResponseWriter, rq *http.Req
 	}
 
 	mt := rs.MutexRepo[v.ID].GetMetrics(v.MType, v.ID)
+	fmt.Printf("Метрик стало: %d шт", len((rs.MutexRepo)))
 	metricsJSON, err := mt.MarshalMetrica()
 	if err != nil {
 		fmt.Println(err.Error())
@@ -245,6 +248,7 @@ func (rs *RepStore) HandlerValueMetricaJSON(rw http.ResponseWriter, rq *http.Req
 	//	log.Fatal(err)
 	//}
 	//fmt.Printf("Пришл текст: %s", b)
+	fmt.Printf("Количество метрик: %d", len((rs.MutexRepo)))
 
 	v := encoding.Metrics{}
 	if err := json.NewDecoder(rq.Body).Decode(&v); err != nil {
