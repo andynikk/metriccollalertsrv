ea4cc5e8667436227ed8b5147ad15756a48deff5
andrey.mikhailov <andynik76@yandex.ru>
2022-08-26 13:02:59 +0300

    Revert "Increment 5. Тестирование 1.021"
    This reverts commit 02be02f8e7e93065ea0b8c1b30dd0aa6b384684f.

--- internal/handlers/server.go
+++ internal/handlers/server.go
@@ -273,6 +273,7 @@ func (rs *RepStore) HandlerValueMetricaJSON(rw http.ResponseWriter, rq *http.Req
 }
 
 func (rs *RepStore) HandleFunc(rw http.ResponseWriter, rq *http.Request) {
+
 	defer rq.Body.Close()
 	rw.WriteHeader(http.StatusOK)
 }
