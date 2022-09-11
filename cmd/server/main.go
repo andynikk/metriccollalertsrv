aa119613c240ee4ae015515630176b4aab31b0f8
andrey.mikhailov <andynik76@yandex.ru>
2022-09-11 15:35:07 +0300

    Спринт 3. Ревью 1. Тест 011

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
+	"github.com/andynikk/metriccollalertsrv/internal/constants"
 	"net/http"
 	"os"
 	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/andynikk/metriccollalertsrv/internal/handlers"
@@ -34,55 +35,37 @@ func Shutdown(rs *handlers.RepStore) {
 
 func main() {
 
-	//rs := handlers.NewRepStore()
-	//
-	//fmt.Println("-*-*", 4, rs.Config.TypeMetricsStorage)
-	//if rs.Config.Restore {
-	//	switch rs.Config.TypeMetricsStorage {
-	//	case constants.MetricsStorageDB:
-	//		rs.LoadStoreMetricsFromDB()
-	//	case constants.MetricsStorageFile:
-	//		rs.LoadStoreMetricsFromFile()
-	//	}
-	//}
-	//fmt.Println("-*-*", 5, rs.Config.Address)
-	//ctx, cancel := context.WithCancel(context.Background())
-	//go BackupData(rs, ctx, cancel)
-	//
-	//fmt.Println("-*-*", 6, rs.Config.Address)
-	//go func() {
-	//	s := &http.Server{
-	//		Addr:    rs.Config.Address,
-	//		Handler: rs.Router}
-	//	fmt.Println("******", s.Addr)
-	//
-	//	if err := s.ListenAndServe(); err != nil {
-	//		constants.Logger.Error().Err(err)
-	//		return
-	//	}
-	//}()
-	//
-	//stop := make(chan os.Signal, 1)
-	//signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
-	//<-stop
-	//Shutdown(rs)
-
 	rs := handlers.NewRepStore()
 
+	fmt.Println("-*-*", 4, rs.Config.TypeMetricsStorage)
+	if rs.Config.Restore {
+		switch rs.Config.TypeMetricsStorage {
+		case constants.MetricsStorageDB:
+			rs.LoadStoreMetricsFromDB()
+		case constants.MetricsStorageFile:
+			rs.LoadStoreMetricsFromFile()
+		}
+	}
+	fmt.Println("-*-*", 5, rs.Config.Address)
+	ctx, cancel := context.WithCancel(context.Background())
+	go BackupData(rs, ctx, cancel)
+
+	fmt.Println("-*-*", 6, rs.Config.Address)
 	go func() {
 		s := &http.Server{
-			Addr:    "localhost:8080",
+			Addr:    rs.Config.Address,
 			Handler: rs.Router}
+		fmt.Println("******", s.Addr)
 
 		if err := s.ListenAndServe(); err != nil {
-			fmt.Printf("%+v\n", err)
+			constants.Logger.Error().Err(err)
 			return
 		}
 	}()
 
-	stop := make(chan os.Signal, 1024)
-	signal.Notify(stop, os.Interrupt) //, os.Kill)
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
 	<-stop
-	log.Panicln("server stopped")
+	Shutdown(rs)
 
 }
