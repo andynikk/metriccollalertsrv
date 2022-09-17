be13450774b5bdc9beb6f0468c447f1d87e6ffcb
andrey.mikhailov <andynik76@yandex.ru>
2022-09-17 08:57:38 +0300

    Спринт 4. Инкремент 14. Тест 017.001

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -109,7 +109,7 @@ func (a *agent) metrixOtherScan() {
 		case <-saveTicker.C:
 
 			cpuUtilization, _ := cpu.Percent(2*time.Second, false)
-			swapMemory, err := mem.SwapMemory()
+			swapMemory, err := mem.SwapMemoryWithContext(ctx)
 			if err != nil {
 				constants.Logger.ErrorLog(err)
 			}
@@ -121,7 +121,7 @@ func (a *agent) metrixOtherScan() {
 			a.data.mx.Lock()
 
 			a.data.metricsGauge["TotalMemory"] = repository.Gauge(swapMemory.Total)
-			a.data.metricsGauge["FreeMemory"] = repository.Gauge(rand.Float64()) //repository.Gauge(swapMemory.Free)
+			a.data.metricsGauge["FreeMemory"] = repository.Gauge(swapMemory.Free)
 			a.data.metricsGauge["CPUutilization1"] = CPUutilization1
 
 			a.data.mx.Unlock()
