59d84400f6cc82efccbaaa4b22daaa3b124910a2
andrey.mikhailov <andynik76@yandex.ru>
2023-01-20 22:39:31 +0300

    Ревью 4.001-3

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -15,19 +15,25 @@ var buildDate = "N/A"
 var buildCommit = "N/A"
 
 func main() {
-
+	fmt.Println("+++++++++++++++ start agent", 1)
 	fmt.Printf("Build version: %s\n", buildVersion)
 	fmt.Printf("Build date: %s\n", buildDate)
 	fmt.Printf("Build commit: %s\n", buildCommit)
 
+	fmt.Println("+++++++++++++++ start agent", 2)
 	config := environment.InitConfigAgent()
-
+	fmt.Println("+++++++++++++++ start agent", 3)
 	a := agent.NewAgent(config)
+	fmt.Println("+++++++++++++++ start agent", 4)
 	a.Run()
+	fmt.Println("+++++++++++++++ start agent", 5)
 
 	stop := make(chan os.Signal, 1)
+	fmt.Println("+++++++++++++++ start agent", 6)
 	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
+	fmt.Println("+++++++++++++++ start agent", 7)
 	<-stop
+	fmt.Println("+++++++++++++++ start agent", 8)
 
 	a.Stop()
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
@@ -10,13 +11,19 @@ import (
 )
 
 func main() {
-
+	fmt.Println("+++++++++++++++ start server", 1)
 	config := environment.InitConfigServer()
+	fmt.Println("+++++++++++++++ start server", 2)
 	srv := handlers.NewServer(config)
+	fmt.Println("+++++++++++++++ start server", 3)
 	srv.Run()
+	fmt.Println("+++++++++++++++ start server", 4)
 
 	stop := make(chan os.Signal, 1)
+	fmt.Println("+++++++++++++++ start server", 5)
 	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
+	fmt.Println("+++++++++++++++ start server", 6)
 	<-stop
+	fmt.Println("+++++++++++++++ start server", 7)
 	srv.Shutdown()
 }
