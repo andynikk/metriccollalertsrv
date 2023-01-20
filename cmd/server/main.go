55cb591aedf7e46cea366f83b6e76a4c0f0cb447
andrey.mikhailov <andynik76@yandex.ru>
2023-01-20 23:06:03 +0300

    Ревью 4.002-1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -15,6 +15,7 @@ var buildDate = "N/A"
 var buildCommit = "N/A"
 
 func main() {
+
 	fmt.Printf("Build version: %s\n", buildVersion)
 	fmt.Printf("Build date: %s\n", buildDate)
 	fmt.Printf("Build commit: %s\n", buildCommit)
